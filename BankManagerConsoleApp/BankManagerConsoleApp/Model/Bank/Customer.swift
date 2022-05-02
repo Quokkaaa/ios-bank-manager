3c80b084cc2d4e8b82e2480657b34bb9ae251170
doogie97 <chris_97@naver.com>
2022-05-02 11:04:09 +0900

    💎feat : Customer에 task프로퍼티 생성, 이에 필요한 Task 열거형 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/Customer.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank/Customer.swift
@@ -4,6 +4,13 @@
 //
 //  Created by 쿼카, 두기 on 2022/04/28.
 //
+
+enum Task {
+    case deposit
+    case loan
+}
+
 struct Customer {
-    let number: Int 
+    let number: Int
+    let task: Task
 }
